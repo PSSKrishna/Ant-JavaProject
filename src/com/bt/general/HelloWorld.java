ad82b29df26c87d385612b83fd9cef0d794aa305
PSSKrishna <33820552+PSSKrishna@users.noreply.github.com>
2018-10-29 18:21:38 +0530

    Update HelloWorld.java

--- src/com/bt/general/HelloWorld.java
+++ src/com/bt/general/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to ANT !! " + getLocalCurrentDate());
-		System.out.println()"Code has changed");
+		
 	}
 
 	private static Date getLocalCurrentDate() {
